9b9ad343f3032e9230f51d6e4e4d5c75567955d4
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-16 19:47:13 +0530

    first react app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import FirstComponent from "./FirstComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="center-container">
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
