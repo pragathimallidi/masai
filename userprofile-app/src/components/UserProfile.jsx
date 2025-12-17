ab6ff4df827e0c1f4bb4ebbf0083c94942c09803
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-17 17:50:14 +0530

    Parent child components with props

---
+++ userprofile-app/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserInfo(props) {
+  return (
+    <div>
+      <p><strong>Name:</strong> {props.name}</p>
+      <p><strong>Age:</strong> {props.age}</p>
+    </div>
+  );
+}
+
+export default UserInfo;
