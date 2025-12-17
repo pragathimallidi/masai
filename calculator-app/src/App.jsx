8f7475b988fc73eb068a7c3d2636c396183ef1bd
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-17 17:37:03 +0530

    Basic calculator

---
+++ calculator-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div>
+      <h1>React Assignment</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
