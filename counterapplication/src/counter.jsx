990a961d0067cc1e24dcb1f056751a4a41c80d49
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-19 19:01:20 +0530

    counter application

---
+++ counterapplication/src/counter.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]); // effect runs ONLY when count changes
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
