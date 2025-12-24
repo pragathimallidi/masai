51877e7a1c22c699c0b3c2bf5eaf8ce27ddf526e
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-24 19:05:57 +0530

    basic react routing

---
+++ basic-routing/src/pages/Todos.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function Todos() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data.slice(0, 10)));
+  }, []);
+
+  return (
+    <div className="page">
+      <h2>Todos</h2>
+      <div className="todo-grid">
+        {todos.map((todo) => (
+          <div className="todo-card" key={todo.id}>
+            <h4>{todo.title}</h4>
+            <p>
+              Status:{" "}
+              <strong>
+                {todo.completed ? "Completed" : "Not Completed"}
+              </strong>
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Todos;
