eedd693a3bb6a0b058dbe801b86a850811d2f3ad
pragathimallidi <pragathimallidi794@gmail.com>
2025-12-24 19:38:30 +0530

    react app

---
+++ react-routing/src/pages/TodoDetails.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const TodoDetails = () => {
+  const { todoId } = useParams();
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+      .then((res) => res.json())
+      .then((data) => setTodo(data));
+  }, [todoId]);
+
+  if (!todo) return <p>Loading...</p>;
+
+  return (
+    <div className="container">
+      <h2>Todo Details</h2>
+      <p><b>ID:</b> {todo.id}</p>
+      <p><b>Title:</b> {todo.title}</p>
+      <p>
+        <b>Status:</b>{" "}
+        {todo.completed ? "Completed ✅" : "Not Completed ❌"}
+      </p>
+    </div>
+  );
+};
+
+export default TodoDetails;
